f22bd2624cffc2e87fa4a1b85767defba19a0481
Rafael Waiti G. Umetsu <rafael.umetsu@gmail.com>
2018-09-28 00:46:22 -0300

    Add files via upload
    Versão com gráfico

---
+++ Pontofixo.py
@@ -0,0 +1,36 @@
+from __future__ import division
+import matplotlib.pyplot as plt
+
+
+def f(x):
+    return (x**3) - (9*x) + 5
+
+
+def g(x):
+    return ((x**3) + 5) / 9
+
+
+def ponto_fixo(x, error, num, graf):
+    for i in range(num):
+        fxn = f(x)
+        graf.append(x)
+        
+        if abs(fxn) < error:
+            return x
+        else:
+            x = g(x)
+
+    print("NÚMERO MÁXIMO DE ITERAÇÕES!")
+
+
+x = 0.75
+error = 1e-2
+num = 50
+graf = []
+
+aux = ponto_fixo(x, error, num, graf)
+
+plt.plot(graf)
+plt.show
+
+print(aux)
