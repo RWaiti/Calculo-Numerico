5125d8f3310b70f84b44a440098e39abebe880ae
lispcheech <filipe.khalifa@gmail.com>
2018-11-08 03:09:13 -0200

    Atualização do metodo de newton

--- modulo2/Método de newton.py
+++ modulo2/Método de newton.py
@@ -8,26 +8,33 @@ Created on Wed Oct 31 18:38:50 2018
 import numpy as np
 
 def d(x,y,n):
-    f = np.zeros((n,n));
+    f = np.zeros((n,n), dtype = "double");
     f[:,0] = y;
-    d = np.zeros(n+1)
+    d = np.zeros(n)
     for i in range(1,n):
         for j in range(i,n):        
             f[j][i] = ((f[j][i-1] - f[j-1][i-1])/(x[j] - x[j-1]))
             #f([j][i]) = 
     
     print(f)
-       # if(i == 1):
-        #    d[i] = f[i]
-         #   p = p + d[i]*(x+1)
-        #if(i > 1):
-         #   p = p + f[i]
+    p = np.array([f[0,0]], dtype="double")  
+    paux = np.array([-x[0],1], dtype="double")  
+    p.resize(2)  
+    p = p + f[1,1]*paux  
+    paux = np.polymul(paux,[-x[1],1])  
+    p.resize(3)  
+    p = p + f[2,2]*paux  
+    paux = np.polymul(paux,[-x[2],1])  
+    p.resize(4)  
+    p = p + f[3,3]*paux
+    print(p)
         
     #return p
 
 
-x = np.matrix([-1,0,2])
-y = np.matrix([4,1,-1])
+
+x = [-1,0,2]
+y = [4,1,-1]
 n = 3
 d(x,y,n)
 
