59f66afbbcbd8fedfb540008cabcd533b514aff2
lipscheech <filipe.khalifa@gmail.com>
2018-11-24 20:22:44 -0200

    Atualização das derivadas numericas

--- modulo3/derivada numérica.py
+++ modulo3/derivada numérica.py
@@ -7,25 +7,42 @@ Created on Wed Nov 21 19:38:11 2018
 import numpy as np
 
 def f(x):
-    return ((5*x**2 + 3*x -9)/(x**2 + 5))
+    return np.exp(-x)
 
-x = 5
-h = 0.01
+def fl(x):
+    return 2*np.cos(2*x) - 2*x
+
+x = 1.5
+h = 1e-2
 #progressiva de ordem 1
 dp1 = (f(x+h) - f(x))/h
-print("Progressiva de primeira ordem: ", dp1)
+erroabs1 = np.abs(fl(x)-dp1)
+errorelat1 = np.abs(erroabs1 / dp1)
+errotruc = (f(x+h) - f(x)) / h - dp1
+print("Diferenças progressivas: ", dp1)
+print("Erro absoluto: ", erroabs1)
+print("Erro relativo: ", errorelat1)
+print("Erro de trucamento: ", errotruc)
 #regressiva de ordem 1
 dr1 = (f(x) - f(x-h))/h
-print("Regressiva de primeira ordem: ", dr1)
+erroabs2 = np.abs(fl(x)-dr1)
+errorelat2 = np.abs(erroabs1 / dr1)
+print("Diferenças regressivas: ", dr1)
+print("Erro absoluto: ", erroabs2)
+print("Erro relativo: ", errorelat2)
 #central de ordem 2
 dc2 = (f(x+h) - f(x-h))/(2*h)
-print("Central de segunda ordem: ", dc2)
+erroabs3 = np.abs(fl(x)-dc2)
+errorelat3 = np.abs(erroabs3 / dc2)
+print("Diferenças centrais: ", dc2)
+print("Erro absoluto: ", erroabs3)
+print("Erro relativo: ", errorelat3)
 #progressiva de ordem 2
 dp2 = (-3*f(x) + 4*f(x+h) - f(x+2*h))/(2*h)
-print("Progressiva de segunda ordem:", dp2)
+#print("Progressiva de segunda ordem:", dp2)
 #regressiva de ordem 2
 dr2 = (f(x-2*h) - 4*f(x-h) + 3*f(x)) / (2*h)
-print("Regressiva de segunda ordem: ", dr2)
+#print("Regressiva de segunda ordem: ", dr2)
 #central de ordem 4
 dc4 = (f(x-2*h) - 8*f(x-h) + 8*f(x+h) - f(x+2*h)) / (12*h)
-print("Central de quarta ordem: ", dc4)
+#print("Central de quarta ordem: ", dc4)
